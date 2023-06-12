24814528ad307593ce852b3efbe1ac07ebbc08b0
ricq99161 <ariqsalsabil@gmail.com>
2023-06-12 17:11:25 +0700

    ini index4

---
+++ index4.js
@@ -0,0 +1,18 @@
+//operator if
+
+//let num1 = 5;
+//if (num1 > 5) {
+   // console.log("num1 lebih dari 5")
+//} else {
+   // console.log("num1 kurang dari 5")
+//}
+
+
+let name = "ariq";
+let role = "student";
+   if (role === student) {
+    console.log("name" + " silahkan masuk");
+   }
+    
+
+  
